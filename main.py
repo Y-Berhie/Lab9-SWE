a361c51a8e66f783857a04df5b4d781af7711780
Y-Berhie <Berhie.Yepeth@gmail.com>
2024-07-25 15:57:15 -0400

    Implement Main function

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def main():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+        option = int(input('Please enter an option: '))
+        if option == 1:
+            password = input('Please enter the password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!\n')
+        elif option == 2:
+            print(f'The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.\n')
+        elif option == 3:
+            break
+
+
 def encode(password):
     int_password = []
     for num in password:
@@ -10,6 +28,7 @@ def encode(password):
         encoded_password += str(i)
     return encoded_password
 
+
 def decode(password):
     newstr = ""
     for num in password:
@@ -18,4 +37,7 @@ def decode(password):
         else:
             newstr += str(int(num) - 3)
     return newstr
-    
+
+
+if __name__ == '__main__':
+    main()
