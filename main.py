5d3069112a49e4edb23ec6faae4faac97da58133
avrrepos <avrrichards@gmail.com>
2024-07-25 13:37:06 -0400

    Implemented decode function

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ def encode(password):
     for i in int_password:
         encoded_password += str(i)
     return encoded_password
+
+def decode(password):
+    newstr = ""
+    for num in password:
+        if int(num) <= 2:
+            newstr += str((int(num) - 3) + 10)
+        else:
+            newstr += str(int(num) - 3)
+    return newstr
+    
