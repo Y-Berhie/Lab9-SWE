1656d83f3d87ae2221ffd5b2729178fb8198106f
Y-Berhie <Berhie.Yepeth@gmail.com>
2024-07-25 12:59:40 -0400

    Implement Encode Function

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-def main():
-    while True:
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit\n')
-        option = int(input('Please enter an option: '))
-        if option == 1:
-            password = input('Please enter the password to encode: ')
-            encoded_password = encode(password)
-            print('Your password has been encoded and stored!\n')
-        elif option == 2:
-            print(f'The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.\n')
-        elif option == 3:
-            break
-
-
 def encode(password):
     int_password = []
     for num in password:
@@ -27,19 +9,3 @@ def encode(password):
     for i in int_password:
         encoded_password += str(i)
     return encoded_password
-
-
-def decode(password):
-    int_password = []
-    for num in password:
-        if int(num) > 2:
-            int_password.append(int(num) - 3)
-        elif int(num) <= 2:
-            int_password.append((int(num) + 10) - 3)
-    decoded_password = ""
-    for i in int_password:
-        decoded_password += str(i)
-    return decoded_password
-
-
-main()
